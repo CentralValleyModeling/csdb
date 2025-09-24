6e0371841f804acb059c067c2656e6edefd47c3c
Zachary Roy <zachary.roy@water.ca.gov>
2025-09-24 15:10:25 -0700

    Reconfigure GH Actions to limit matrix, add tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -31,8 +31,26 @@ def test_read_single_timeseries_from_dss(
     dss: Path,
     request: pytest.FixtureRequest,
 ):
-    single_timeseries_dss = request.getfixturevalue(dss)
-    df = csdb.io.load_dss(single_timeseries_dss)
+    timeseries_dss = request.getfixturevalue(dss)
+    df = csdb.io.load_dss(timeseries_dss)
     assert list(df.columns) == ["datetime", "variable", "value"]
     assert len(df) == 1212
+    assert df["datetime"].max() == pd.to_datetime("2021-09-30")
+
+
+@pytest.mark.parametrize(
+    "dss",
+    [
+        ("multi_timeseries_dss_7"),
+        ("multi_timeseries_dss_6"),
+    ],
+)
+def test_read_multi_timeseries_from_dss(
+    dss: Path,
+    request: pytest.FixtureRequest,
+):
+    timeseries_dss = request.getfixturevalue(dss)
+    df = csdb.io.load_dss(timeseries_dss)
+    assert list(df.columns) == ["datetime", "variable", "value"]
+    assert len(df) == 1212 + 1200
     assert df["datetime"].max() == pd.to_datetime("2021-09-30")
